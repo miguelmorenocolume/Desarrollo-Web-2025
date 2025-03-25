9da8d2c05eb8d1f0768cc5e945086574b8792f45
Miguel Moreno <mmorenocolume@gmail.com>
2025-03-25 10:52:17 +0100

    commit dia 38

---
+++ Semana 10/Ejercicios dia 38/Ejercicio 2/script.js
@@ -0,0 +1,10 @@
+var squares = document.getElementById("square1");
+var eyeball = document.getElementById("eyeball");
+
+document.addEventListener("mousemove", event => {
+    let posX = event.clientX - window.innerWidth / 2;
+    let posY = event.clientY - window.innerHeight / 2;
+
+    squares.style.transform = "translate(" + posX * 0.01 + "%, " + posY * 0.01 + "%)";
+    eyeball.style.transform = "translate(" + posX * 0.08 + "%, " + posY * 0.08 + "%)";
+});
